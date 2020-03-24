3919418d94a72c6cf36a5c96740b8d1e86ddf5e6
阿波 <alex.hiutung@gmail.com>
2020-03-24 16:51:14 +0800

    自定义Zuul过滤器

---
+++ one-common/src/main/java/com/wuxianmali/one/common/entity/OneConstant.java
@@ -0,0 +1,13 @@
+package com.wuxianmali.one.common.entity;
+
+public class OneConstant {
+
+    /**
+     * Zuul请求头TOKEN名称（不要有空格）
+     */
+    public static final String ZUUL_TOKEN_HEADER = "ZuulToken";
+    /**
+     * Zuul请求头TOKEN值
+     */
+    public static final String ZUUL_TOKEN_VALUE = "one:zuul:123456";
+}
