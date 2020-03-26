3895fbe2b857ae891f7534c5e481aa75e4f7b2ba
bxd <xiaodong.me@gmail.com>
2020-03-26 22:27:48 +0800

    system模块集成MyBatis Plus

---
+++ one-server/one-server-system/src/main/java/com/wuxianmali/one/server/system/config/OneWebConfigure.java
@@ -0,0 +1,23 @@
+package com.wuxianmali.one.server.system.config;
+
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class OneWebConfigure {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        List<ISqlParser> sqlParserList = new ArrayList<>();
+        sqlParserList.add(new BlockAttackSqlParser());
+        paginationInterceptor.setSqlParserList(sqlParserList);
+        return paginationInterceptor;
+    }
+}
